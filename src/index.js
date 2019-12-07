e00bbe68674cf2c824392fd2c7095fbf906a2dae
Tisha Holder <50963507+TishaHolder@users.noreply.github.com>
2019-12-07 00:53:51 -0500

    debugging

--- src/index.js
+++ src/index.js
@@ -8,16 +8,18 @@ import { Provider } from "react-redux";
 import reducer from "./reducers/reducer.js";
 
 //Before creating the store, check localStorage and parse any JSON under your key like this:
-const persistedState = localStorage.getItem('reduxState') ? JSON.parse(localStorage.getItem('reduxState')) : {}
+//const persistedState = localStorage.getItem('reduxState') ? JSON.parse(localStorage.getItem('reduxState')) : {}
 
 //You then pass this peristedState constant to your createStore method like this
-const store = createStore(reducer, persistedState);
+//const store = createStore(reducer, persistedState);
+
+const store = createStore(reducer);
 
 //every time the value of todoState changes we call localStorage.setItem and give it the key 
 //to store the value under
-store.subscribe(()=>{
-        localStorage.setItem('reduxState', JSON.stringify(store.getState()))
-})
+//store.subscribe(()=>{
+        //localStorage.setItem('reduxState', JSON.stringify(store.getState()))
+//})
 
 ReactDOM.render(
 <Provider store = {store} > 
